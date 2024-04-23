f1482ee52cc7cc6ff475665143b54e1e8356bd72
jongin-kim <tlddbs1@naver.com>
2024-04-23 11:48:44 +0900

    first commit

---
+++ src/main/java/com/switchwon/SwitchwonApplication.java
@@ -0,0 +1,13 @@
+package com.switchwon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwitchwonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwitchwonApplication.class, args);
+    }
+
+}
